717c61abec6b1c25eb0f795fa97d406c126e982a
Trevor Heslop <sythe.zn@gmail.com>
2017-05-10 09:41:12 +0200

    feat: added BST structure and insert, rotate methods

---
+++ statsd.net-Tests/BinaryStringTreeTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using statsd.net.shared;
+
+namespace statsd.net_Tests
+{
+  [TestClass]
+  public class BinaryStringTreeTests
+  {
+    [TestMethod]
+    public void FirstInsertShouldSetRootNode()
+    {
+      var testValue = "test";
+
+      var bst = new BinaryStringTree();
+      bst.Insert(testValue);
+
+      Assert.AreSame(bst.RootNode.Value, testValue);
+    }
+
+    [TestMethod]
+    public void Comparison_A_ShouldBe_LeftOf_B()
+    {
+      var a = "a";
+      var b = "b";
+
+      var bst = new BinaryStringTree();
+      bst.Insert(b);
+      bst.Insert(a);
+
+      Assert.AreSame(bst.RootNode.Left.Value, a);
+    }
+
+    [TestMethod]
+    public void Comparison_B_ShouldBe_RightOf_A()
+    {
+      var a = "a";
+      var b = "b";
+
+      var bst = new BinaryStringTree();
+      bst.Insert(a);
+      bst.Insert(b);
+
+      Assert.AreSame(bst.RootNode.Right.Value, b);
+    }
+
+    [TestMethod]
+    public void Rotation_ABC_ShouldBe_RootB_LeftA_RightC()
+    {
+      var a = "a";
+      var b = "b";
+      var c = "c";
+
+      var bst = new BinaryStringTree();
+      bst.Insert(a);
+      bst.Insert(b);
+      bst.Insert(c);
+
+      Assert.AreSame(bst.RootNode.Value,b);
+      Assert.AreSame(bst.RootNode.Left.Value, a);
+      Assert.AreSame(bst.RootNode.Right.Value, c);
+    }
+
+    [TestMethod]
+    public void Rotation_CBA_ShouldBe_RootB_LeftA_RightC()
+    {
+      var a = "a";
+      var b = "b";
+      var c = "c";
+
+      var bst = new BinaryStringTree();
+      bst.Insert(c);
+      bst.Insert(b);
+      bst.Insert(a);
+
+      Assert.AreSame(bst.RootNode.Value, b);
+      Assert.AreSame(bst.RootNode.Left.Value, a);
+      Assert.AreSame(bst.RootNode.Right.Value, c);
+    }
+
+    [TestMethod]
+    public void Rotation_ABCDEF_ShouldBe_RootB_LeftA_RightC()
+    {
+      var a = "a";
+      var b = "b";
+      var c = "c";
+      var d = "d";
+      var e = "e";
+      var f = "f";
+      var g = "g";
+
+      var bst = new BinaryStringTree();
+      bst.Insert(a);
+      bst.Insert(b);
+      bst.Insert(c);
+      bst.Insert(d);
+      bst.Insert(e);
+      bst.Insert(f);
+      bst.Insert(g);
+
+      // given input a b c d e f g
+      // output should be:
+      //                           
+      //        d                   
+      //      b   f                  
+      //     a c e g                   
+
+      Assert.AreSame(bst.RootNode.Value, d);
+
+      Assert.AreSame(bst.RootNode.Left.Value, b);
+      Assert.AreSame(bst.RootNode.Right.Value, f);
+
+      Assert.AreSame(bst.RootNode.Left.Left.Value, a);
+      Assert.AreSame(bst.RootNode.Left.Right.Value, c);
+
+      Assert.AreSame(bst.RootNode.Right.Left.Value, e);
+      Assert.AreSame(bst.RootNode.Right.Right.Value, g);
+    }
+
+  }
+}
--- statsd.net.shared/BinaryStringTree.cs
+++ statsd.net.shared/BinaryStringTree.cs
@@ -1,7 +1,14 @@
-﻿namespace statsd.net.shared
+﻿using System;
+using System.Diagnostics;
+using System.Web.UI;
+
+namespace statsd.net.shared
 {
   public class BinaryStringTree
   {
+    private Node _rootNode;
+
+    public Node RootNode => _rootNode;
     public BinaryStringTree()
     {
       
@@ -9,7 +16,119 @@
 
     public void Insert(string value)
     {
-      
+      var currentNode = _rootNode;
+
+      while (currentNode != null)
+      {
+        var compareResult = String.Compare(value, currentNode.Value, StringComparison.InvariantCultureIgnoreCase);
+
+        if (compareResult < 0)
+        {
+          if (currentNode.Left == null)
+          {
+            currentNode.Left = new Node {Parent = currentNode, Value = value};
+            Balance(currentNode, 1);
+            return;
+          }
+          currentNode = currentNode.Left;
+        }
+        else if (compareResult > 0)
+        {
+          if (currentNode.Right == null)
+          {
+            currentNode.Right = new Node() {Parent = currentNode, Value = value};
+            Balance(currentNode, -1);
+            return;
+          }
+          currentNode = currentNode.Right;
+        }
+        else
+        {
+          // duplicate value, nothing to do
+          return;
+        }
+      }
+
+      _rootNode = new Node()
+      {
+        Value = value
+      };
+    }
+
+    private void Balance(Node node, sbyte balanceOffset)
+    {
+      while (node != null)
+      {
+        node.Balance += balanceOffset;
+
+        if (node.Balance == 0)
+          return;
+
+        if (node.Balance == -2) //a
+        {
+          node.Right.Left = node;
+          node.Right.Parent = node.Parent;
+          node.Parent = node.Right;
+          node.Right = null;
+
+          if (node.Parent.Parent == null)
+            _rootNode = node.Parent;
+
+          node.Balance = 0;
+          node.Parent.Balance = 0;
+          return;
+        }
+
+        if (node.Balance == 2)
+        {
+          node.Left.Right = node;
+          node.Left.Parent = node.Parent;
+          node.Parent = node.Left;
+          node.Left = null;
+
+          if (node.Parent.Parent == null)
+            _rootNode = node.Parent;
+
+          node.Balance = 0;
+          node.Parent.Balance = 0;
+          return;
+        }
+
+        if (node.Parent != null)
+        {
+          if (node == node.Parent.Left)
+            balanceOffset = 1;
+          else
+            balanceOffset = -1;
+        }
+
+        node = node.Parent;
+      }
+    }
+
+    public void Delete(string value)
+    {
+      throw new NotImplementedException();
+    }
+
+    public bool Match(string value)
+    {
+      throw new NotImplementedException();
+    }
+
+    public string MatchNearest(string value)
+    {
+      throw new NotImplementedException();
+    }
+
+    [DebuggerDisplay("{Value}")]
+    public class Node
+    {
+      public string Value;
+      public int Balance;
+      public Node Parent;
+      public Node Left;
+      public Node Right;
     }
   }
 }
